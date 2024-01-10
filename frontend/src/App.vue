e214048ca96922c4b2f9d3397b076e42a8a8ccd4
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-01-10 12:16:02 +0100

    part-2 fini

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
   <form class="flex flex-col w-fit m-auto" @submit.prevent="submitForm" @submit="print">
     <div class="flex justify-between">
       <label class="m-3 w-4/12" for="email">Email</label>
-      <input class="border m-3 w-8/12" type="text" v-model="formData.email" placeholder="monemail@gmail.com" name="email">
+      <input class="border m-3 w-8/12" type="email" v-model="formData.email" placeholder="monemail@gmail.com" name="email">
     </div>
     <div class="flex justify-between">
       <label class="m-3 w-4/12" for="category">Category</label>
@@ -29,13 +29,14 @@
       </select>
     </div>
     <div class="flex justify-end">
-      <button class="flex w-fit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Valider</button>
+      <button :disabled="isFormValid" class="flex w-fit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-blue-200" type="submit">Valider</button>
     </div>
   </form>
 </template>
 
 <script setup>
   import { ref } from 'vue'
+  import { computed } from "vue"
 
   const formData = ref ({
     email: "",
@@ -44,9 +45,29 @@
     priority: "",
   })
 
-  const selected = ref('')
+  const isEmailValid = computed ( () => {
+    const re = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    return re.test(formData.value.email)
+  })
 
-  function submitForm() {
-    console.log(formData)
+  const isDescriptionValid = computed ( () => {
+    return formData.value.description !== ""
+  })
+
+  const isCategoryValid = computed ( () => {
+    return formData.value.category !== ""
+  })
+
+  const isPriorityValid = computed ( () => {
+    return formData.value.priority !== ""
+  })
+
+  const isFormValid = computed ( () => {
+    return !(isEmailValid.value && isDescriptionValid.value && isCategoryValid.value && isPriorityValid.value)
+  })
+
+  const submitForm = (evt) => {
+    //console.log(formData)
+    
   }
 </script>
